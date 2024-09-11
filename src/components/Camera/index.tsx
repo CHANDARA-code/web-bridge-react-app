fa747e09e3b068ae20ea85cfcbde55805332a576
chandara-dgc <L.Chandara@dgc.gov.kh>
2024-09-11 10:43:59 +0700

    build: implement new usage

--- src/components/Camera/index.tsx
+++ src/components/Camera/index.tsx
@@ -1,32 +1,55 @@
-import React from "react";
-// import { capturePhoto, capturePhotoBase64, requestCameraPermission } from "../../utils";
+import React, { useState, useEffect } from "react";
 import {
   capturePhoto,
   capturePhotoBase64,
   requestCameraPermission,
+  JavascriptBridgeModel,
+  Base64ContentModel,
 } from "dg-super-app-bridge";
 
 import SizeBox from "../Sizebox";
 
 const Camera: React.FC = () => {
+  const [base64Content, setBase64Content] = useState<string>("");
+
+  useEffect(() => {}, [base64Content]);
+
+  const capture = async () => {
+    try {
+      const result =
+        (await capturePhotoBase64()) as JavascriptBridgeModel<Base64ContentModel>;
+      if (result.success) {
+        setBase64Content("data:image/png;base64," + result.data?.base64 ?? "");
+      }
+    } catch (error) {}
+  };
+
   return (
     <div>
       <h2>Camara</h2>
-
       <button onClick={requestCameraPermission}>
         Request Camera Permission
       </button>
-
       <SizeBox></SizeBox>
-      <button onClick={capturePhotoBase64}>Capture Photo Base64</button>
+      <button onClick={capture}>Capture Photo Base64</button>
       <button onClick={capturePhoto}>Capture Photo</button>
       <SizeBox />
-      <img
-        id="photo"
-        src="https://web-bridge-react-app.netlify.app/1019372.png"
-        alt="Captured Photo"
-        style={{ width: 180 }}
-      />
+      {base64Content && (
+        <img
+          id="photo"
+          src={base64Content}
+          alt="Captured Photo"
+          style={{ width: 180 }}
+        />
+      )}
+      {!base64Content && (
+        <img
+          id="photo"
+          src="https://web-bridge-react-app.netlify.app/1019372.png"
+          alt="Captured Photo"
+          style={{ width: 180 }}
+        />
+      )}
     </div>
   );
 };
--- src/components/OpenLink/index.tsx
+++ src/components/OpenLink/index.tsx
@@ -7,14 +7,21 @@ import React from "react";
 //   selectFile,
 // } from "../../utils";
 
-import { capturePhoto, requestCameraPermission, openLink } from "dg-super-app-bridge";
+import {
+  capturePhoto,
+  requestCameraPermission,
+  openLink,
+} from "dg-super-app-bridge";
 import SizeBox from "../Sizebox";
 
 const OpenLink: React.FC = () => {
+  const kk = async () => {
+    await openLink("https://youtu.be/HpD470sf99I");
+  };
   return (
     <div>
       <h2>Open Link</h2>
-      <button onClick={openLink}>Open Link</button>
+      <button onClick={kk}>Open Link</button>
       <SizeBox></SizeBox>
     </div>
   );
